9edf11f3c9d2e20c8e982cedff298ee38dc4ecec
James D. Nurmi <jdnurmi@qwe.cc>
2018-09-23 19:38:40 -0700

    Don't do unnecessary string conversion

--- fetcher.go
+++ fetcher.go
@@ -87,7 +87,7 @@ func (f *fetch) tryFetchMessage() {
 
 	if err != nil {
 		// If redis returns null, the queue is empty. Just ignore the error.
-		if err.Error() != "redigo: nil returned" {
+		if err != redis.ErrNil {
 			Logger.Println("ERR: ", err)
 			time.Sleep(1 * time.Second)
 		}
